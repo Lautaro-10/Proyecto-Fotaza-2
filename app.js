d91799afa11aee8097d609e942f5b335f1e504b9
Lautaro-10 <laucadelago123@gmail.com>
2026-04-15 21:42:49 -0300

    Configurar vistas a pug y añadir gitignore

--- app.js
+++ app.js
@@ -3,4 +3,16 @@ const app = express();
 
 app.set('view engine', 'pug');
 
-app.listen(3000);
+
+app.get('/',(req, res) => {
+  console.log('Hola mundo');
+  
+  res.render('index.pug')
+})
+
+
+const PORT = process.env.PORT || 3000
+ 
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http:\\localhost:${PORT}`);
+});
