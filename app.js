fa93b79c0d5ebca0269324ef136a8bfc201ecf39
Lautaro-10 <laucadelago123@gmail.com>
2026-05-18 20:37:48 -0300

    migrate HTML templates to Pug and implement routing for application pages

--- app.js
+++ app.js
@@ -18,3 +18,19 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http:\\localhost:${PORT}`);
 });
+
+app.get('/register', (req, res) => {
+  res.render('register.pug');
+});
+
+app.get('/followers', (req, res) => {
+  res.render('followers.pug');
+});
+
+app.get('/our_story', (req, res) => {
+  res.render('our_story.pug');
+});
+
+app.get('/posts', (req, res) => {
+  res.render('posts.pug');
+});
