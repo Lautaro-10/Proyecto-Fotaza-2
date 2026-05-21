2ad6360905ce75ebddbbe01e5939d25c3ac4b7b8
Lautaro-10 <laucadelago123@gmail.com>
2026-05-21 16:39:35 -0300

    Arreglo de rutas en followers,our_story,posts

--- controller/our_story.js
+++ controller/our_story.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/our_story', (req, res) => {
+router.get('/', (req, res) => {
   res.render('our_story.pug');
 });
 
